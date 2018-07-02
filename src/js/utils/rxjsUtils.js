f50135800bcfe33cb34446ba2ab5f24f20c6416f
Julian Gieseke <jgieseke@mesosphere.com>
2018-07-02 11:52:56 +0200

    refactor(rxjsUtils): change linearBackoff interface
    Closes DCOS-38709

--- src/js/utils/rxjsUtils.js
+++ src/js/utils/rxjsUtils.js
@@ -2,18 +2,29 @@ import { timer } from "rxjs/observable/timer";
 
 import "rxjs/add/operator/delayWhen";
 import "rxjs/add/operator/scan";
+// import { Observable } from "rxjs/Observable";
+// import { Scheduler } from "rxjs/Scheduler";
 
 /* eslint-disable import/prefer-default-export */
+/**
+ * linearBackoff callback to use with e.g. delayWhen
+ *
+ * @param  {number} delay
+ * @param  {number} [maxRetries=-1]
+ * @param  {number} [maxDelay=Number.MAX_SAFE_INTEGER]
+ * @param  {Scheduler} [scheduler=null]
+ * @return {Observable} notifier obervable
+ */
 export function linearBackoff(
-  maxRetries = 5,
-  delay = 1000,
+  delay,
+  maxRetries = -1,
   maxDelay = Number.MAX_SAFE_INTEGER,
   scheduler = null
 ) {
   return errors =>
     errors
       .scan((count, error) => {
-        if (count >= maxRetries) {
+        if (maxRetries >= 0 && count >= maxRetries) {
           throw error;
         }
 
